1873fa15da17c31d800cd240bf9acd128db7f53a
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-07-31 21:21:22 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p>Hello Vue</p>
+        <Header />
         <home />
     </div>
 </template>
@@ -8,15 +8,19 @@
 <script>
 
 import Home from './views/Home'
+import Header from './shared-components/Header/MainHeader'
 
 export default {
   components: {
-    Home
+    Home,
+    Header
   }
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap');
+
  * {
     padding: 0;
     margin: 0;
@@ -24,6 +28,7 @@ export default {
 
   html {
     box-sizing: border-box;
+    font-family: 'Play', sans-serif;
   }
 
   *, *::before, *::after {
--- src/shared-components/Header/MainHeader.vue
+++ src/shared-components/Header/MainHeader.vue
@@ -1,11 +1,34 @@
 <template>
-    <section>
-        <p>Logo</p>
+    <section class='header'>
+        <router-link to='/' tag='p'>Logo</router-link>
         <nav>
-            <li>Home</li>
-            <li>Something</li>
-            <li>Account</li>
-            <li>Login</li>
+            <router-link to='/' tag='li'>Home</router-link>
+            <router-link to='/' tag='li'>Something</router-link>
+            <router-link to='/account' tag='li'>Account</router-link>
+            <router-link to='/login' tag='li'>Login</router-link>
         </nav>
     </section>
 </template>
+
+<style lang="scss" scoped>
+    .header {
+        padding: 25px 45px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        p{
+            cursor: pointer;
+        }
+        nav {
+            display: flex;
+            width: 30%;
+            justify-content: space-between;
+            li {
+                list-style: none;
+                text-transform: uppercase;
+                font-size: 14px;
+                cursor: pointer;
+            }
+        }
+    }
+</style>
