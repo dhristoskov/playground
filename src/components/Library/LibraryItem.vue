4266511d0d71b9745b345086a534719da707f203
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-08-04 16:33:17 +0200

    commit

--- src/components/Library/LibraryItem.vue
+++ src/components/Library/LibraryItem.vue
@@ -1,8 +1,28 @@
 <template>
+  <div class="sond-item">
+    <!-- Add photo of every song -->
     <p>{{ song.name }}</p>
+    <p> - {{ song.singer }}</p>
+    <p>{{ song.duration }} s.</p>
+    <div class="buttons">
+      <p><i class="fas fa-play-circle"></i></p>
+      <!-- this is <i class="fas fa-stop-circle"></i> <-- stop button -->
+      <p><i class="fas fa-plus-circle"></i></p>
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
   props: ['song']
 }
 </script>
+
+<style lang="scss" scoped>
+  .sond-item {
+    display: flex;
+    .buttons {
+      display: flex;
+    }
+  }
+</style>
--- src/shared-components/data/musicData.js
+++ src/shared-components/data/musicData.js
@@ -1,12 +1,12 @@
 export const musicData = [
-  { id: '01', name: 'Head & Heart', singer: 'Joel Corry' },
-  { id: '02', name: 'Savage Love (Laxed - Siren Beat)', singer: 'Jawsh 685 & Jason Derulo' },
-  { id: '03', name: 'Rockstar', singer: 'Dababy ft. Roddy Ricch' },
-  { id: '04', name: 'Lighter', singer: 'Nathan Dawe ft. Ksi' },
-  { id: '05', name: 'Only you freestyle', singer: 'Headie one & Drake' },
-  { id: '06', name: 'Cardigan', singer: 'Taylor Swift' },
-  { id: '07', name: 'Watermelon suger', singer: 'Harry Styles' },
-  { id: '08', name: 'Exile', singer: 'Taylor Swift ft. Bon Iver' },
-  { id: '09', name: 'West Ten', singer: 'AJ Tracey & Mabel' },
-  { id: '10', name: 'The 1', singer: 'Taylor Swift' }
+  { id: '01', name: 'Head & Heart', singer: 'Joel Corry', duration: 3.25 },
+  { id: '02', name: 'Savage Love (Laxed - Siren Beat)', singer: 'Jawsh 685 & Jason Derulo', duration: 4.05 },
+  { id: '03', name: 'Rockstar', singer: 'Dababy ft. Roddy Ricch', duration: 4.11 },
+  { id: '04', name: 'Lighter', singer: 'Nathan Dawe ft. Ksi', duration: 4.46 },
+  { id: '05', name: 'Only you freestyle', singer: 'Headie one & Drake', duration: 3.13 },
+  { id: '06', name: 'Cardigan', singer: 'Taylor Swift', duration: 5.17 },
+  { id: '07', name: 'Watermelon suger', singer: 'Harry Styles', duration: 4.21 },
+  { id: '08', name: 'Exile', singer: 'Taylor Swift ft. Bon Iver', duration: 3.49 },
+  { id: '09', name: 'West Ten', singer: 'AJ Tracey & Mabel', duration: 4.01 },
+  { id: '10', name: 'The 1', singer: 'Taylor Swift', duration: 5.07 }
 ]
