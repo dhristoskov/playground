e318dcdcebdb76a6b9f58e506d888435863c0f35
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-08-09 23:00:48 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
